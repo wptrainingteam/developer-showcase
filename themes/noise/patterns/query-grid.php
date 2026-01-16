fdbf77904024ecf47ded41c0e38f8463f86badec
Justin Tadlock <justintadlock@gmail.com>
2026-01-16 16:05:31 -0600

    Better spacing for query and pagination.
    Note that we need to use `!important` to fix this margin for the Query Pagination block in the site editor.

--- themes/noise/patterns/query-grid.php
+++ themes/noise/patterns/query-grid.php
@@ -16,55 +16,62 @@ declare(strict_types=1);
 defined('ABSPATH') || exit;
 
 ?>
-<!-- wp:query {
-	"metadata":{"name":"<?= esc_attr__('Posts Query', 'developer-showcase-noise') ?>"},
-	"queryId":0,
-	"query":{
-		"perPage":6,
-		"pages":0,
-		"offset":0,
-		"postType":"post",
-		"order":"desc",
-		"orderBy":"date",
-		"author":"",
-		"search":"",
-		"exclude":[],
-		"sticky":"",
-		"inherit":true
-	},
+<!-- wp:group {
 	"align":"full",
 	"style":{
 		"spacing":{
 			"padding":{
 				"top":"var:preset|spacing|70",
-				"right":"var:preset|spacing|70",
 				"bottom":"var:preset|spacing|70",
-				"left":"var:preset|spacing|70"
+				"left":"var:preset|spacing|70",
+				"right":"var:preset|spacing|70"
 			}
 		}
 	},
-	"layout":{"type":"constrained"}
+	"layout":{"type":"default"}
 } -->
-<div class="wp-block-query alignfull" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--70)">
 
-	<!-- wp:post-template {
-		"style":{"spacing":{"blockGap":"var:preset|spacing|70"}},
-		"align":"full",
-		"layout":{"type":"grid","columnCount":3}
+	<!-- wp:query {
+		"metadata":{"name":"<?= esc_attr__('Posts Query', 'developer-showcase-noise') ?>"},
+		"queryId":0,
+		"query":{
+			"perPage":6,
+			"pages":0,
+			"offset":0,
+			"postType":"post",
+			"order":"desc",
+			"orderBy":"date",
+			"author":"",
+			"search":"",
+			"exclude":[],
+			"sticky":"",
+			"inherit":true
+		}
 	} -->
+	<div class="wp-block-query">
 
-		<!-- wp:pattern {"slug":"developer-showcase-noise/post-excerpt"} /-->
+		<!-- wp:post-template {
+			"style":{"spacing":{"blockGap":"var:preset|spacing|70"}},
+			"align":"full",
+			"layout":{"type":"grid","columnCount":3}
+		} -->
 
-	<!-- /wp:post-template -->
+			<!-- wp:pattern {"slug":"developer-showcase-noise/post-excerpt"} /-->
 
-	<!-- wp:query-pagination {
-		"paginationArrow":"arrow",
-		"layout":{"type":"flex"}
-	} -->
-		<!-- wp:query-pagination-previous /-->
-		<!-- wp:query-pagination-numbers /-->
-		<!-- wp:query-pagination-next /-->
-	<!-- /wp:query-pagination -->
+		<!-- /wp:post-template -->
+
+		<!-- wp:query-pagination {
+			"paginationArrow":"arrow",
+			"layout":{"type":"flex"}
+		} -->
+			<!-- wp:query-pagination-previous /-->
+			<!-- wp:query-pagination-numbers /-->
+			<!-- wp:query-pagination-next /-->
+		<!-- /wp:query-pagination -->
+
+	</div>
+	<!-- /wp:query -->
 
 </div>
-<!-- /wp:query -->
+<!-- /wp:group -->
