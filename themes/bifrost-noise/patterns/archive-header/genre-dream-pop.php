9b601f70b560cc4d0cb84186a411da3eeb64540b
Justin Tadlock <justintadlock@gmail.com>
2026-03-02 15:43:50 -0600

    Move album and genre archive templates to patterns.
    Still needs cleanup. TODO: Dynamic images from the theme.

---
+++ themes/bifrost-noise/patterns/archive-header/genre-dream-pop.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Title: Dream Pop Archive Header
+ * Slug: bifrost-noise/archive-header-genre-dream-pop
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:cover {"url":"http://localhost:8884/wp-content/uploads/2026/02/genre-dream-pop.png","id":90,"hasParallax":true,"dimRatio":80,"overlayColor":"black","isUserOverlayColor":true,"sizeSlug":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|0","left":"var:preset|spacing|0"}}},"layout":{"type":"constrained","contentSize":"80rem"}} -->
+<div class="wp-block-cover has-parallax" style="padding-right:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><div class="wp-block-cover__image-background wp-image-90 size-full has-parallax" style="background-position:50% 50%;background-image:url(http://localhost:8884/wp-content/uploads/2026/02/genre-dream-pop.png)"></div><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-80 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+		<div class="wp-block-group"><!-- wp:group {"className":"is-style-meta","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground-on-accent"}}},"spacing":{"blockGap":"var:preset|spacing|40"}},"textColor":"foreground-on-accent","fontSize":"2-xs","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group is-style-meta has-foreground-on-accent-color has-text-color has-link-color has-2-xs-font-size"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:outermost/icon-block {"iconName":"","iconColor":"foreground-on-accent","iconColorValue":"#ffffff","width":"1.38em"} -->
+					<div class="wp-block-outermost-icon-block"><div class="icon-container has-icon-color has-foreground-on-accent-color" style="color:#ffffff;width:1.38em;transform:rotate(0deg) scaleX(1) scaleY(1)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M485-275q35-35 35-85v-280h120v-80H460v256q-14-8-29-12t-31-4q-50 0-85 35t-35 85q0 50 35 85t85 35q50 0 85-35Zm-5 195q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"></path></svg></div></div>
+					<!-- /wp:outermost/icon-block -->
+
+					<!-- wp:paragraph -->
+					<p>Genre</p>
+					<!-- /wp:paragraph --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph -->
+				<p>//</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"bifrost-music/term","args":{"field":"count"}}}}} -->
+				<p>0</p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
+
+			<!-- wp:term-description /--></div>
+		<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
