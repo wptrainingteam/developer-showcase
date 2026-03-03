a579d77fdddd3bb65259353a5493bb977574297d
Justin Tadlock <justintadlock@gmail.com>
2026-03-03 09:42:46 -0600

    Moves remaining archive templates to patterns.
    Still need to update dynamic content.

---
+++ themes/bifrost-noise/patterns/archive-header/category-artist-spotlight.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Title: Artist Spotlight Category Archive Header
+ * Slug: bifrost-noise/archive-header-category-artist-spotlight
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:cover {"url":"http://localhost:8884/wp-content/uploads/2026/02/category-artist-spotlight.png","id":151,"hasParallax":true,"dimRatio":80,"overlayColor":"black","isUserOverlayColor":true,"sizeSlug":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|0","left":"var:preset|spacing|0"}}},"layout":{"type":"constrained","contentSize":"80rem"}} -->
+<div class="wp-block-cover has-parallax" style="padding-right:var(--wp--preset--spacing--0);padding-left:var(--wp--preset--spacing--0)"><div class="wp-block-cover__image-background wp-image-151 size-full has-parallax" style="background-position:50% 50%;background-image:url(http://localhost:8884/wp-content/uploads/2026/02/category-artist-spotlight.png)"></div><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-80 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+		<div class="wp-block-group"><!-- wp:group {"className":"is-style-meta","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground-on-accent"}}},"spacing":{"blockGap":"var:preset|spacing|40"}},"textColor":"foreground-on-accent","fontSize":"2-xs","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group is-style-meta has-foreground-on-accent-color has-text-color has-link-color has-2-xs-font-size"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+				<div class="wp-block-group"><!-- wp:outermost/icon-block {"iconName":"","iconColor":"foreground-on-accent","iconColorValue":"#ffffff","width":"1.38em"} -->
+					<div class="wp-block-outermost-icon-block"><div class="icon-container has-icon-color has-foreground-on-accent-color" style="color:#ffffff;width:1.38em;transform:rotate(0deg) scaleX(1) scaleY(1)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M740-560h140v80h-80v220q0 42-29 71t-71 29q-42 0-71-29t-29-71q0-42 29-71t71-29q8 0 18 1.5t22 6.5v-208ZM120-160v-112q0-35 17.5-63t46.5-43q62-31 126-46.5T440-440q42 0 83.5 6.5T607-414q-20 12-36 29t-28 37q-26-6-51.5-9t-51.5-3q-57 0-112 14t-108 40q-9 5-14.5 14t-5.5 20v32h321q2 20 9.5 40t20.5 40H120Zm207-367q-47-47-47-113t47-113q47-47 113-47t113 47q47 47 47 113t-47 113q-47 47-113 47t-113-47Zm169.5-56.5Q520-607 520-640t-23.5-56.5Q473-720 440-720t-56.5 23.5Q360-673 360-640t23.5 56.5Q407-560 440-560t56.5-23.5ZM440-640Zm0 400Z"></path></svg></div></div>
+					<!-- /wp:outermost/icon-block -->
+
+					<!-- wp:paragraph -->
+					<p>Category</p>
+					<!-- /wp:paragraph --></div>
+				<!-- /wp:group -->
+
+				<!-- wp:paragraph -->
+				<p>//</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"bifrost-music/term","args":{"field":"count"}}}}} -->
+				<p>0</p>
+				<!-- /wp:paragraph --></div>
+			<!-- /wp:group -->
+
+			<!-- wp:query-title {"type":"archive","showPrefix":false} /-->
+
+			<!-- wp:term-description /--></div>
+		<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
