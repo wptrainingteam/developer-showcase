fd9d977ba15fe75e54beb68678223650118bdd25
Justin Tadlock <justintadlock@gmail.com>
2026-03-05 16:52:15 -0600

    Generic archive template handling.

--- themes/bifrost-noise/patterns/archive-header/index.php
+++ themes/bifrost-noise/patterns/archive-header/index.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Title: Search Header
- * Slug: bifrost-noise/archive-header-search
+ * Title: Default Archive Header
+ * Slug: bifrost-noise/archive-header-index
  * Inserter: no
  */
 
@@ -11,7 +11,7 @@ declare(strict_types=1);
 # Prevent direct access.
 defined('ABSPATH') || exit;
 
-$image = get_theme_file_uri('public/media/images/archive/search.png');
+$image = get_theme_file_uri('public/media/images/archive/default.png');
 
 ?>
 <!-- wp:cover {"url":"<?= esc_url($image) ?>","id":92,"hasParallax":true,"dimRatio":80,"overlayColor":"black","isUserOverlayColor":true,"sizeSlug":"large","style":{"spacing":{"padding":{"right":"var:preset|spacing|0","left":"var:preset|spacing|0"}}},"layout":{"type":"constrained","contentSize":"80rem"}} -->
@@ -20,11 +20,11 @@ $image = get_theme_file_uri('public/media/images/archive/search.png');
 			<div class="wp-block-group is-style-meta has-foreground-on-accent-color has-text-color has-link-color has-2-xs-font-size"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 				<div class="wp-block-group">
 					<!-- wp:outermost/icon-block {"iconName":"","iconColor":"foreground-on-accent","iconColorValue":"#ffffff","width":"1.38em"} -->
-					<div class="wp-block-outermost-icon-block"><div class="icon-container has-icon-color has-foreground-on-accent-color" style="color:#ffffff;width:1.38em;transform:rotate(0deg) scaleX(1) scaleY(1)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"></path></svg></div></div>
+					<div class="wp-block-outermost-icon-block"><div class="icon-container has-icon-color has-foreground-on-accent-color" style="color:#ffffff;width:1.38em;transform:rotate(0deg) scaleX(1) scaleY(1)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M130-189 81-546q-5-32 15.5-58t52.5-31l61 435 283-40h267q-8 21-24.5 35.5T695-187l-477 66q-33 5-58-15t-30-53Zm190-127q-33 0-56.5-23.5T240-396v-364q0-33 23.5-56.5T320-840h480q33 0 56.5 23.5T880-760v364q0 33-23.5 56.5T800-316H320Zm0-80h480v-364H320v364Zm0 0v-364 364ZM210-200Zm190-400h320v-80H400v80Zm0 120h200v-80H400v80Z"/></svg></div></div>
 					<!-- /wp:outermost/icon-block -->
 
 					<!-- wp:paragraph -->
-					<p>Search</p>
+					<p>Archive</p>
 					<!-- /wp:paragraph --></div>
 				<!-- /wp:group -->
 
@@ -37,13 +37,7 @@ $image = get_theme_file_uri('public/media/images/archive/search.png');
 				<!-- /wp:paragraph --></div>
 			<!-- /wp:group -->
 
-			<!-- wp:query-title {"type":"search","align":"wide"} /-->
-
-			<!-- wp:paragraph {"fontSize":"sm","fontFamily":"tertiary"} -->
-			<p class="has-tertiary-font-family has-sm-font-size">Explore matches across our entire catalog, including artists, releases, editorial articles, and curated playlists.</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Type something here...","width":75,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside"} /-->
+			<!-- wp:query-title {"type":"archive","align":"wide"} /-->
 		</div>
 		<!-- /wp:group --></div></div>
 <!-- /wp:cover -->
