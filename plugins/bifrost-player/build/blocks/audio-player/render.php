932bf1d569cc100a2a2bb8cdfebd5699d188ee19
JuanMa <juanma.garrido@gmail.com>
2026-04-14 18:30:46 +0100

    Add persistent audio player block with server-side rendering and styles
    - Implemented server rendering for the audio player block in `render.php`.
    - Added RTL and LTR styles for the audio player in `style-index-rtl.css` and `style-index.css`.
    - Created asset file for the audio player block in `view.asset.php`.
    - Developed JavaScript functionality for the audio player in `view.js`, including waveform visualization and playback controls.

---
+++ plugins/bifrost-player/build/blocks/audio-player/render.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Server rendering for the persistent audio player block.
+ *
+ * @package bifrost-player
+ */
+
+declare(strict_types=1);
+
+wp_interactivity_state('bifrost-player', [
+	'currentTrackUrl'    => '',
+	'currentTrackTitle'  => '',
+	'currentTrackArtist' => '',
+	'currentTrackImage'  => '',
+	'isAudioPlaying'     => false,
+]);
+
+$wrapper_attributes = get_block_wrapper_attributes([
+	'class' => 'bifrost-audio-player',
+]);
+?>
+
+<div
+	<?php echo $wrapper_attributes; ?>
+	data-wp-interactive="bifrost-player"
+	data-wp-bind--hidden="!state.isPlayerVisible"
+	data-wp-watch="callbacks.syncPlayback"
+	hidden
+>
+	<div class="bifrost-audio-player__inner">
+		<div class="bifrost-audio-player__artwork">
+			<img
+				data-wp-bind--src="state.currentTrackImage"
+				alt=""
+				width="48"
+				height="48"
+			/>
+		</div>
+
+		<div class="bifrost-audio-player__info">
+			<span
+				class="bifrost-audio-player__title"
+				data-wp-text="state.currentTrackTitle"
+			></span>
+			<span
+				class="bifrost-audio-player__artist"
+				data-wp-text="state.currentTrackArtist"
+			></span>
+		</div>
+
+		<div
+			class="bifrost-audio-player__waveform"
+			data-wp-watch="callbacks.syncWaveform"
+		></div>
+
+		<div class="bifrost-audio-player__controls">
+			<button
+				class="bifrost-audio-player__close"
+				data-wp-on--click="actions.closePlayer"
+				aria-label="<?php esc_attr_e('Close player', 'bifrost-player'); ?>"
+			>
+				<?php esc_html_e('✕', 'bifrost-player'); ?>
+			</button>
+		</div>
+	</div>
+
+	<audio
+		class="bifrost-audio-player__audio"
+		data-wp-bind--src="state.currentTrackUrl"
+		data-wp-on--ended="actions.onTrackEnded"
+		preload="metadata"
+	></audio>
+</div>
---
+++ plugins/bifrost-player/build/blocks/audio-player/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'a64fd9d2fc3076a6119f', 'type' => 'module');
